1cb10425a9bea854c57b181e0734acc7018300e8
Ola Söderström <ola.soderstrom@nttcomsecurity.com>
2016-07-07 22:35:07 +0200

    Added support for weighted distance

--- leven/levenshtein_impl.h
+++ leven/levenshtein_impl.h
@@ -18,10 +18,18 @@
 #ifndef levenshtein_impl_h
 #define levenshtein_impl_h
 
-#include <algorithm>
-#include <climits>
-#include <stdexcept>
+#include <set>
 #include <vector>
+#include <climits>
+#include <algorithm>
+#include <stdexcept>
+
+const bool BLACKLIST = false;
+const bool WHITELIST = true;
+const char* const BLACKLIST_CHARS = ";:,'";
+const char* const WHITELIST_CHARS = "ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvxyz1234567890&=/_-?. ";
+const int PENALTY = 2; 
+
 
 static unsigned min(unsigned a, unsigned b, unsigned c)
 {
@@ -31,6 +39,19 @@ static unsigned min(unsigned a, unsigned b, unsigned c)
 template <typename Char>
 static unsigned levenshtein(Char const *a, size_t m, Char const *b, size_t n)
 {
+
+    std::set<char> chars;
+
+    if (BLACKLIST == true) {
+        char const * weight_chars = BLACKLIST_CHARS;
+        chars.insert(weight_chars, weight_chars + strlen(weight_chars));
+    }
+    
+    if (WHITELIST == true) {
+        char const * weight_chars = WHITELIST_CHARS;
+        chars.insert(weight_chars, weight_chars + strlen(weight_chars));
+    }
+
     // Swap a and b if necessary to ensure m <= n.
     if (m > n) {
         std::swap(a, b);
@@ -43,7 +64,7 @@ static unsigned levenshtein(Char const *a, size_t m, Char const *b, size_t n)
         m--, n--;
     }
 
-    // Skip common suffix.
+    
     while (m > 0 && n > 0 && a[m - 1] == b[n - 1]) {
         m--, n--;
     }
@@ -66,17 +87,32 @@ static unsigned levenshtein(Char const *a, size_t m, Char const *b, size_t n)
     for (size_t i = 0; i <= m; i++) {
         cur[i] = i;
     }
+
     for (size_t j = 1; j <= n; j++) {
         std::swap(cur, prev);
-
         cur[0] = j;
 
         for (size_t i = 1; i <= m; i++) {
+            size_t w = 1;
+
+            if (BLACKLIST == true) {
+                if ( (chars.find(a[i - 1]) != chars.end()) || (chars.find(b[j - 1]) != chars.end()) ) {
+                    w = PENALTY;
+                }
+            }
+
+            if (WHITELIST == true) {
+                if ( (chars.find(a[i - 1]) == chars.end()) || (chars.find(b[j - 1]) == chars.end()) ) {
+                    w = PENALTY;
+                }
+            }
+            
+
             if (a[i - 1] == b[j - 1]) {
                 cur[i] = prev[i - 1];
             }
             else {
-                cur[i] = min(cur[i - 1], prev[i], prev[i - 1]) + 1;
+                cur[i] = min(cur[i - 1], prev[i], prev[i - 1]) + w;
             }
         }
     }
